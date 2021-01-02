62b2c91cf82d934e203e6ffe55aaf9d2f8386dcc
linux-flower <76842257+linux-flower@users.noreply.github.com>
2021-01-02 16:36:18 -0300

    clock.py
    Melhoria no código.

--- PythonTest/clock.py
+++ PythonTest/clock.py
@@ -1,26 +1,29 @@
 from tkinter import *
-import tkinter
 from datetime import datetime
 
-#Colors
-black = "#3d3d3d" #Preto
-white = "#fafcff" #Branco
-green = "#21c25c" #Verde
-red =  "#eb463b" #Vermelho
-grey = "#dedcdc" #Cinza
-blue = "#3080f0" #Azul
+# Colors
+black: str = "#3d3d3d"  # Preto
+white: str = "#fafcff"  # Branco
+green: str = "#21c25c"  # Verde
+red: str = "#eb463b"  # Vermelho
+grey: str = "#dedcdc"  # Cinza
+blue: str = "#3080f0"  # Azul
 
-wallpeper = white
+wallpeper: str = white
 color = black
 
-window=Tk()
+window = Tk()
 window.title("Digital Clock")
-window.geometry("440x180")
+window.geometry("380x150")
 window.resizable(width=FALSE, height=FALSE)
 window.configure(bg=white)
 
+bt = Button(window, width=20, text='OK')
+bt.place(x=100, y=150)
+
+
 def clock():
-    time=datetime.now()
+    time = datetime.now()
 
     hour = time.strftime("%H:%M:%S")
     week_day = time.strftime("%A")
@@ -30,12 +33,13 @@ def clock():
 
     text.config(text=hour)
     text.after(1000, clock)
-    text1.config(text=week_day + " "+ str(day) + "/" + str(mont) + "/" + str(year))
+    text1.config(text=week_day + " " + str(day) + "/" + str(mont) + "/" + str(year))
 
-text=Label(window, text="", font=("Arial 70 italic"), bg=wallpeper, fg=color)
+
+text = Label(window, text="", font='Arial 70 ', bg=wallpeper, fg=color)
 text.grid(row=0, column=0, stick=NW, padx=5)
 
-text1=Label(window, text="", font=("Arial 20 italic"), bg=wallpeper, fg=color)
+text1 = Label(window, text="", font="Arial 20 italic", bg=wallpeper, fg=color)
 text1.grid(row=1, column=0, stick=NW, padx=5)
 
 clock()
